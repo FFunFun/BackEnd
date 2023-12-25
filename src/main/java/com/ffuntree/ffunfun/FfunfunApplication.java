e9be84f5107b966bd96829160fe3c43255006cb3
uknow-home <yoon6763@naver.com>
2023-12-25 18:50:41 +0900

    Initial commit: spring project

---
+++ src/main/java/com/ffuntree/ffunfun/FfunfunApplication.java
@@ -0,0 +1,13 @@
+package com.ffuntree.ffunfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfunfunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FfunfunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ffuntree/ffunfun/FfunfunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ffuntree.ffunfun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FfunfunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
