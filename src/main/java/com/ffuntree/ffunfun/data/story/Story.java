37dd49b5b6941751101d10d844f3d984eefe33c5
uknow-home <yoon6763@naver.com>
2024-03-19 01:19:50 +0900

    Feat: Story 엔티티 추가

---
+++ src/main/java/com/ffuntree/ffunfun/data/story/Story.java
@@ -0,0 +1,33 @@
+package com.ffuntree.ffunfun.data.story;
+
+import com.ffuntree.ffunfun.data.ffun.FFunRoom;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity
+public class Story {
+
+    @Id
+    @Column(name = "story_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long storyId;
+
+    private String title;
+
+    private String content;
+
+    private LocalDateTime datetime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ffun_room_id")
+    private FFunRoom ffunRoom;
+
+}
